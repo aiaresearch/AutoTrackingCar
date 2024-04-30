fd8a99947821bfa00a982a84d4741caad9e55750
黄智轩 <155860789+HZX080814@users.noreply.github.com>
2024-04-30 23:13:27 +0800

    start spot_label

---
+++ spot_label/draw.py
@@ -0,0 +1,33 @@
+import cv2
+import numpy as np
+import random
+
+# # 创建一个空白图像
+# image = np.zeros((300, 400, 3), dtype=np.uint8)
+
+# # 绘制直线
+# start_point = (50, 50)
+# end_point = (350, 250)
+# color = (0, 0, 0)  # BGR 颜色
+# thickness = 2
+# cv2.line(image, start_point, end_point, color, thickness)
+
+# # 绘制圆弧
+# center = (200, 150)
+# axes = (100, 50)
+# angle = 0
+# start_angle = 0
+# end_angle = 180
+# color = (0, 0, 255)  # BGR 颜色
+# thickness = 2
+# cv2.ellipse(image, center, axes, angle, start_angle, end_angle, color, thickness)
+
+height = 480
+width = 640
+image = np.full((height, width, 3), 255, dtype=np.uint8)
+# image = np.zeros((height, width, 3), dtype=np.uint8)
+# image[:, :] = (255,255,255)
+# 显示图像
+cv2.imshow("Image", image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
---
+++ spot_label/rename.py
@@ -0,0 +1,3 @@
+import os
+dir_path = os.listdir(os.path.join(os.path.dirname(__file__), "images"))
+print(dir_path)
